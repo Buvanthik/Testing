3bb4adf12df3e3eaf759a27bb71d9419449e0044
karth <karth@Buvanthik>
2022-09-07 07:22:21 +0530

    hi

---
+++ Besant/src/main/java/pom/besant/Besant/App.java
@@ -0,0 +1,13 @@
+package pom.besant.Besant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Besant/src/main/java/pom/besant/Besant/Testing.java
@@ -0,0 +1,29 @@
+package pom.besant.Besant;
+
+import org.testng.annotations.Test;
+
+public class Testing  {
+
+	@Test 
+
+	public void signup() {
+		System.out.println("Sign Up");
+	}
+
+	@Test 
+	public void login() {
+		System.out.println("Login");
+	}
+
+	@Test
+
+	public void searchProduct() {
+		System.out.println("Search Product");
+	}
+
+	@Test
+	public void addToCart() {
+		System.out.println("Add to Cart");
+	}
+
+}
